7ecb85f214c67cadcc3cb45f5649b8fa55ebc484
Vrushali Pistulkar <vpistulkar@adobe.com>
2024-01-24 23:58:40 +0530

    hero-img changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -63,7 +63,7 @@ async function loadEager(doc) {
   const main = doc.querySelector('main');
   if (main) {
     decorateMain(main);
-    document.body.classList.add('appear');
+   document.body.classList.add('appear');
     await waitForLCP(LCP_BLOCKS);
   }
 
