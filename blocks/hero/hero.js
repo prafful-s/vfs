3eebab1cd61a33bfde363d3b3b06e36a6d3bd67c
Vrushali Pistulkar <vpistulkar@adobe.com>
2024-01-24 18:53:24 +0530

    Fix issue hero and alignmennt

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -10,7 +10,24 @@ export default function decorate(block) {
         const picWrapper = pic.closest('div');
           if (picWrapper && picWrapper.children.length === 1) {
             // picture is only content in column
-              picWrapper.classList.add('hero1-img-col');
+            picWrapper.classList.add('hero1-img-col');
+            var heroImgColElements = document.getElementsByClassName("hero1-img-col");
+                      for (var i = 0; i < heroImgColElements.length; i++) {
+                        // Get the immediate parent div of the current "hero1-img-col" element
+                        var parentDiv = heroImgColElements[i].parentElement;
+                    
+                        // Add a new class to the parent div
+                        parentDiv.classList.add("image-wrapper");
+                      }
+                   
+                     var imageWrapperElements = document.getElementsByClassName("image-wrapper");
+
+              
+                      for (var i = 0; i < imageWrapperElements.length; i++) {
+                        var siblingDiv = imageWrapperElements[i].nextElementSibling;
+                        siblingDiv.classList.add("text-wrapper");
+                      }
+
             }
           }
       });
